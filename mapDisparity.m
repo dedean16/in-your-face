d744f6c9c886006291c0a0266f2706d475baeb75
Daniel <danielcox16@hotmail.com>
2017-04-11 16:10:44 +0200

    Combine disparity RGB channels

--- mapDisparity.m
+++ mapDisparity.m
@@ -3,29 +3,51 @@ function disparmap = mapDisparity(im1, im2)
 % Disparity is calculated for each color separately
 visualise = true;
 
+% Calculate disparity map for each color separately
 for rgb = 1:3                               % Loop over RGB index (1, 2 or 3)
     img1 = im1(:,:,rgb);
     img2 = im2(:,:,rgb);
 
     % Calculate disparity map
     disparRange = [0 640];
-    disparmap(:,:,rgb) = disparity(img1, img2,...
+    disparmap_rgb(:,:,rgb) = disparity(img1, img2,...
         'BlockSize', 5, 'DisparityRange', disparRange);
-    disparmap(disparmap<0) = NaN;
     
-    disparmap(disparmap<200 | disparmap>400) = NaN;       % remove other stuff
+    % Assume: face values are in predefined range
+    disparmap_rgb(disparmap_rgb<150 | disparmap_rgb>450) = 0;       % remove other stuff
 end
 
-%% Visualisation
+%% Combine color channels into one image: mean of nonzero values
+zeromap    = sum(disparmap_rgb == 0, 3);
+summap     = sum(disparmap_rgb, 3);
+disparmap    = summap./(3 - zeromap);
+
+% Visualisation
 if visualise
     figure
     
-    % Show disparity map
-    subplot(1,2,1)
-    imshow((disparmap-200)/200);
-    title('Disparity map');
+    % Show disparity map RGB
+    subplot(2,2,1)
+    imshow((disparmap_rgb-200)/200);
+    title('Disparity map - RGB');
     
     % Show histogram
-    subplot(1,2,2)
-    hist(disparmap(:),40)
+    subplot(2,2,2)
+    histvals = disparmap_rgb(:);
+    histvals(histvals == 0) = NaN;
+    hist(histvals,40)
+    title('Disparity Histogram')
+    
+    % Show disparity map
+    subplot(2,2,3)
+    imshow(zeromap,[1 3]);
+    title('Disparity map - Zeromap');
+    
+    % Show disparity map
+    subplot(2,2,4)
+    imshow(disparmap,[]);
+    title('Mean Disparity map');
 end
+
+
+
