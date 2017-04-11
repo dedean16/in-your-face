795a1f8829fc2ddf3767b079c692f60ce47b76d7
Daniel <danielcox16@hotmail.com>
2017-04-11 15:11:38 +0200

    Select disparity face range
    Face disparty points all seem to be in the range of 200 to 400 in this specific case. Only this range is selected.

--- mapDisparity.m
+++ mapDisparity.m
@@ -3,22 +3,29 @@ function disparmap = mapDisparity(im1, im2)
 % Disparity is calculated for each color separately
 visualise = true;
 
-% [W,H] = size(im1);                      % Get image size
-% disparmap = zeros(W,H,3);               % Initialize disparity map
-
-for rgb = 1:3                           % Loop over RGB index (1, 2 or 3)
+for rgb = 1:3                               % Loop over RGB index (1, 2 or 3)
     img1 = im1(:,:,rgb);
     img2 = im2(:,:,rgb);
 
     % Calculate disparity map
-    disparRange = [200 360];
+    disparRange = [0 640];
     disparmap(:,:,rgb) = disparity(img1, img2,...
         'BlockSize', 5, 'DisparityRange', disparRange);
+    disparmap(disparmap<0) = NaN;
+    
+    disparmap(disparmap<200 | disparmap>400) = NaN;       % remove other stuff
 end
 
-% Visualisation
+%% Visualisation
 if visualise
     figure
-    imshow(disparmap,disparRange);
-    title('Disparity Map');
+    
+    % Show disparity map
+    subplot(1,2,1)
+    imshow((disparmap-200)/200);
+    title('Disparity map');
+    
+    % Show histogram
+    subplot(1,2,2)
+    hist(disparmap(:),40)
 end
