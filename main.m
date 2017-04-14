3e3959dbc9960cd004acbfc571d96af82ac1f050
Roni Hytonen <roni.hytonen@aalto.fi>
2017-04-14 11:17:00 +0200

    Added calibrations and modified main

--- main.m
+++ main.m
@@ -6,22 +6,24 @@
 PROJ_DIR = [PROJ_DIR, '/'];
 cd(PROJ_DIR);
 
-addpath(PROJ_DIR);
+addpath(genpath(PROJ_DIR));
 
 %% Start by camera calibration
 
-% load PROJ_DIR/ipcv_project3/'Calibratie 1'/calibrationLeft/LeftCamParam/
-% load PROJ_DIR/ipcv_project3/'Calibratie 1'/calibrationMiddle/MidCamParam/
-% load PROJ_DIR/ipcv_project3/'Calibratie 1'/calibrationRight/RightCamParam/
+    % Define subject and calibration paths
+    SUBJ = 'subject2';      % Possibilities 1,2,4
+    CALIB= 'Calibratie 1/'; % Possibilities 1,2
 
-    % Load stereo calibration for Middle and Right
-load([PROJ_DIR, 'Calibratie 1/sP_MR.mat'])
-load([PROJ_DIR, 'Calibratie 1/sP_ML.mat'])
+    % Load stereo calibration 
+load([PROJ_DIR, CALIB, 'sP_MR.mat'])
+load([PROJ_DIR, CALIB, 'sP_ML.mat'])
 
     % Load face images
-im_L = imread([PROJ_DIR,'subject1/subject1Left/subject1_Left_1.jpg']);
-im_M = imread([PROJ_DIR,'subject1/subject1Middle/subject1_Middle_1.jpg']);
-im_R = imread([PROJ_DIR,'subject1/subject1Right/subject1_Right_1.jpg']);
+im_L = imread([PROJ_DIR, SUBJ, '/', SUBJ, 'Left/', SUBJ, '_Left_1.jpg']);
+im_M = imread([PROJ_DIR, SUBJ, '/', SUBJ, 'Middle/', SUBJ, '_Middle_1.jpg']);
+im_R = imread([PROJ_DIR, SUBJ, '/', SUBJ, 'Right/', SUBJ, '_Right_1.jpg']);
+% im_M = imread([PROJ_DIR,'subject1/subject1Middle/subject1_Middle_1.jpg']);
+% im_R = imread([PROJ_DIR,'subject1/subject1Right/subject1_Right_1.jpg']);
 
 figure(1); clf
     subplot(1,3,1);
@@ -36,9 +38,20 @@ figure(1); clf
 %% Remove background in im_*_noBG and store the mask to mask_*
 %   (in mask_* BG pixels are 0 and face 1)
 
-[im_L_noBG, mask_L] = removeBackground(im_L, 0.10, 2, false);
-[im_M_noBG, mask_M] = removeBackground(im_M, 0.10, 2, false);
-[im_R_noBG, mask_R] = removeBackground(im_R, 0.10, 2, false);
+switch SUBJ(end);
+    case '1'
+        edgeThresh  = 0.1;
+        strelVal    = 2;
+    case '2'
+        edgeThresh  = 0.05;
+        strelVal    = 4;
+end
+    
+
+plotFlag = true;
+[im_L_noBG, mask_L] = removeBackground(im_L, 0.05, 4, plotFlag);
+[im_M_noBG, mask_M] = removeBackground(im_M, 0.05, 4, plotFlag);
+[im_R_noBG, mask_R] = removeBackground(im_R, 0.05, 4, plotFlag);
 
 figure; 
     imshow(im_L_noBG);
@@ -56,8 +69,11 @@ im_R_norm = normalizeImages(im_R_noBG, im_M_noBG);
     
 %% Rectify
 
-[im_Mr_rec, im_R_rec] = rectifyStereoImages(im_M_noBG, im_R_noBG, sP_MR, 'OutputView','full');
-[im_Ml_rec, im_L_rec] = rectifyStereoImages(im_M_noBG, im_L_noBG, sP_ML, 'OutputView','full');
+
+[im_Mr_rec, im_R_rec] = rectifyStereoImages(im_M_noBG, im_R_norm, sP_MR,...
+                            'OutputView','full');
+[im_Ml_rec, im_L_rec] = rectifyStereoImages(im_M_noBG, im_L_norm, sP_ML,...
+                            'OutputView','full');
 
 figure(2); clf
     subplot(1,2,1);
