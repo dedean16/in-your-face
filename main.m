150bf02bac0fba9c7cecc4d2669c0528b02e7752
Roni Hytonen <roni.hytonen@aalto.fi>
2017-04-22 13:09:01 +0200

    Improved masks

--- main.m
+++ main.m
@@ -17,13 +17,13 @@ addpath(genpath(PROJ_DIR));
     % Load stereo calibration 
 load([PROJ_DIR, CALIB, 'sP_MR.mat'])
 load([PROJ_DIR, CALIB, 'sP_ML.mat'])
+load([PROJ_DIR, CALIB, 'sP_LR.mat'])
 
     % Load face images
 im_L = imread([PROJ_DIR, SUBJ, '/', SUBJ, 'Left/', SUBJ, '_Left_1.jpg']);
 im_M = imread([PROJ_DIR, SUBJ, '/', SUBJ, 'Middle/', SUBJ, '_Middle_1.jpg']);
 im_R = imread([PROJ_DIR, SUBJ, '/', SUBJ, 'Right/', SUBJ, '_Right_1.jpg']);
-% im_M = imread([PROJ_DIR,'subject1/subject1Middle/subject1_Middle_1.jpg']);
-% im_R = imread([PROJ_DIR,'subject1/subject1Right/subject1_Right_1.jpg']);
+
 
 figure(1); clf
     subplot(1,3,1);
@@ -51,7 +51,7 @@ switch SUBJ(end);
 end
     
 
-plotFlag = true;
+plotFlag = false;
 [im_L_noBG, mask_L] = removeBackground(im_L, edgeThresh, strelVal, plotFlag);
 [im_M_noBG, mask_M] = removeBackground(im_M, edgeThresh, strelVal, plotFlag);
 [im_R_noBG, mask_R] = removeBackground(im_R, edgeThresh, strelVal, plotFlag);
@@ -72,42 +72,68 @@ im_R_norm = normalizeImages(im_R, im_M);
     
 %% Rectify
 
+OutpView = 'full';
 
 [im_Mr_rec, im_R_rec] = rectifyStereoImages(im_M, im_R_norm, sP_MR,...
-                            'OutputView','full');
+                            'OutputView',OutpView);
 [im_Ml_rec, im_L_rec] = rectifyStereoImages(im_M, im_L_norm, sP_ML,...
-                            'OutputView','full');
+                            'OutputView',OutpView);
+[im_Lr_rec, im_Rl_rec] = rectifyStereoImages(im_L, im_R_norm, sP_LR,...
+                            'OutputView',OutpView);
 
 [mask_Mr_rec, mask_R_rec] = rectifyStereoImages(mask_M, mask_R, sP_MR,...
-                            'OutputView','full');
+                            'OutputView',OutpView);
 [mask_Ml_rec, mask_L_rec] = rectifyStereoImages(mask_M, mask_L, sP_ML,...
-                            'OutputView','full');
+                            'OutputView',OutpView);
+[mask_Lr_rec, mask_Rl_rec] = rectifyStereoImages(mask_L, mask_R, sP_LR,...
+                            'OutputView',OutpView);
 
                         
-figure(2); clf
-    subplot(1,2,1);
-        imshow(stereoAnaglyph(im_Mr_rec, im_R_rec));
-	subplot(1,2,2);
-        imshow(stereoAnaglyph(im_Ml_rec, im_L_rec));
+masked_Mr_rec = im_Mr_rec .* uint8(repmat(mask_Mr_rec,[1,1,3]));
+masked_R_rec  = im_R_rec  .* uint8(repmat(mask_R_rec,[1,1,3]));
+                        
+masked_Ml_rec = im_Ml_rec .* uint8(repmat(mask_Ml_rec,[1,1,3]));
+masked_L_rec  = im_L_rec  .* uint8(repmat(mask_L_rec,[1,1,3]));
 
-figure(3); clf
-    subplot(1,2,1);
-        imshow(mask_Mr_rec);
-	subplot(1,2,2);
-        imshow(stereoAnaglyph(mask_Ml_rec, mask_L_rec));
+masked_Lr_rec = im_Lr_rec .* uint8(repmat(mask_Lr_rec,[1,1,3]));
+masked_Rl_rec = im_Rl_rec .* uint8(repmat(mask_Rl_rec,[1,1,3]));
+
+
+
+figure;
+    CreateAxes(3,1,1);
+        imshow(stereoAnaglyph(masked_Mr_rec, masked_R_rec));
+	CreateAxes(3,1,2);
+        imshow(stereoAnaglyph(masked_Ml_rec, masked_L_rec));
+	CreateAxes(3,1,3);
+        imshow(stereoAnaglyph(masked_Lr_rec, masked_Rl_rec));
 
         
+figure;
+    CreateAxes(2,1,1);
+        imshow(mask_Mr_rec);
+	CreateAxes(2,1,2);
+        imshow(mask_Ml_rec);
+	
 
 %% Disparity
 
-disparmap = mapDisparity(im_Mr_rec, im_R_rec);
-    % Cut the background out my using the binary mask
+disparmap_R = mapDisparity(im_Mr_rec, im_R_rec);
+disparmap_L = mapDisparity(im_Ml_rec, im_L_rec);
+    % Cut the background out by using the binary mask
 figure;
-    imshow(disparmap.* mask_Mr_rec)
+    imshow(disparmap_R)
+figure;
+    imshow(disparmap_L)
+    
+facemesh(disparmap_R.* mask_Mr_rec) 
+facemesh(disparmap_L.* mask_Ml_rec) 
+
 
 %%
-disparmap(isnan(disparmap)) = 0;
-disparmap(mask_Mr_rec==1 & disparmap==0) = -1;
-disparmap_f = relaxgaps(disparmap.*mask_Mr_rec,-1,1,300,0.001,0);
-figure; imshow(disparmap_f,[])
+disparmap_R(isnan(disparmap_R)) = 0;
+disparmap_R(mask_Mr_rec==1 & disparmap_R==0) = -1;
+disparmap_f = relaxgaps(disparmap_R.*mask_Mr_rec,-1,1,300,0.001,0);
 
+figure; imshow(disparmap_f,[])
+facemesh(disparmap_f)
