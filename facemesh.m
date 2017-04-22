9fd01b3efc98366492762e940a263015697dff20
Daniel <danielcox16@hotmail.com>
2017-04-22 14:11:54 +0200

    Use pointCloud instead of heightmap mesh
    Represent the data in a pointCloud instead of converting the heightmap to a mesh. This allows better visualisation of the data, as errors in a heightmap mesh show up as big spikes. Furthermore, RGB values of rectified image are used.

--- facemesh.m
+++ facemesh.m
@@ -1,8 +1,19 @@
-function facemesh(disparmap)
+function facemesh(disparmap,rgbmap)
 
 [W,H] = size(disparmap);
 [X,Y] = meshgrid(1:H,1:W);
 
 figure
-surf(X,Y,disparmap,'EdgeColor','none')
+% surf(X,Y,disparmap,'EdgeColor','none')
 
+% Gathering point data
+xyzPoints = [X(:) Y(:) 5*disparmap(:)];
+rmap = rgbmap(:,:,1);
+gmap = rgbmap(:,:,2);
+bmap = rgbmap(:,:,3);
+rgbPoints = [rmap(:) gmap(:) bmap(:)];
+
+% Construct point cloud
+ptCloud = pointCloud(xyzPoints,'Color',rgbPoints);
+pcshow(ptCloud);
+camproj('perspective')
